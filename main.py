5b1bf90ba8caae8d2472ca27395a89892e5103e2
radini5948 <RadekBeta@gmail.com>
2025-05-10 21:39:16 +0200

    bombombini gusini

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from shapely.geometry import box
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, font
+
 
 # Wczytanie danych
 df = pd.read_csv("polska_woj.csv")
 df2 = pd.DataFrame()
+
 
 # Wczytanie mapy
 polska = geopandas.read_file(("wojewodztwa.shp"))
@@ -28,11 +30,11 @@ entry = tk.Entry(window, width=30)
 entry.pack()
 
 # Lista wybranych miast
-#lista_miast = tk.Label(window, text="Wybrane miasta: brak", wraplength=300)
-#lista_miast.pack()
+lista_miast = tk.Label(window, text="", wraplength=300)
+lista_miast.place(x=1200,y=150)
 
 
-ilosc_miast = tk.Label(window, text="Liczba miast: " + "0 / 1024", wraplength=300)
+ilosc_miast = tk.Label(window, text="Liczba miast: " + str(len(df)), wraplength=300)
 ilosc_miast.pack()
 
 # --- Tworzenie figury i canvasu tylko raz ---
@@ -46,8 +48,6 @@ canvas.get_tk_widget().pack()
 # --- Główna funkcja rysująca mapę ---
 def pokaz_miasto():
     global df2
-    global ilosc
-    #ilosc = 0
     miasto = entry.get().strip().lower()  # konwersja do małych liter
 
     # porównanie z kolumną 'miasto', też w małych literach
@@ -62,14 +62,12 @@ def pokaz_miasto():
         gdf = geopandas.GeoDataFrame(
             df2, geometry=geopandas.points_from_xy(df2['dlug'], df2['szer']), crs="EPSG:4326"
         )
-
-        gdf.plot(ax=ax, color="red", markersize=gdf['ludnosc'] / 2000)
-        ax.set_title("Wybrane miasta")
-
+        #ax.set_title("Wybrane miasta")
+        gdf.plot(ax=ax, color="red")
         canvas.draw()
 
         #lista_miast.config(text="Wybrane miasta: " + ", ".join(df2['miasto'].unique()))
-        ilosc_miast.config(text="Ilość miast: " + str(len(df2)) + " / 1011")
+        ilosc_miast.config(text="Ilość miast: " + str(len(df2)) + " / " + str(len(df)))
         entry.delete(0, tk.END)
 
     else:
@@ -81,14 +79,73 @@ def pokaz_miasto():
 def pokaz_miasto_enter(event):
     pokaz_miasto()
 
+def pokaz_populacje():
+    temp_df = df2
+    ax.clear()
+    polska.clip(clip).plot(ax=ax, color="white", edgecolor="black")
+    gdf_2 = geopandas.GeoDataFrame(
+        temp_df, geometry=geopandas.points_from_xy(temp_df['dlug'], temp_df['szer']), crs="EPSG:4326"
+    )
+    gdf_2.plot(ax=ax, color="red", markersize=temp_df['ludnosc'] / 2000)
+    ax.set_title("Wybrane miasta")
+
+    canvas.draw()
+def pokaz_brakujace():
+    temp_df = df
+    temp_df = temp_df.merge(df2, how='left', on=temp_df.columns.tolist(), indicator=True)
+    temp_df = temp_df[temp_df['_merge'] == 'left_only'].drop(columns=['_merge'])
+    ax.clear()
+    polska.clip(clip).plot(ax=ax, color="white", edgecolor="black")
+    gdf_2 = geopandas.GeoDataFrame(
+        temp_df, geometry=geopandas.points_from_xy(temp_df['dlug'], temp_df['szer']), crs="EPSG:4326"
+    )
+
+    gdf_2.plot(ax=ax, color="gray")
+    ax.set_title("Wybrane miasta")
+
+    canvas.draw()
+
+    # lista_miast.config(text="Wybrane miasta: " + ", ".join(df2['miasto'].unique()))
+    ilosc_miast.config(text="Ilość miast brakujących miast: " + str(len(temp_df)) )
+
+def powrot():
+    ax.clear()
+    polska.clip(clip).plot(ax=ax, color="white", edgecolor="black")
+    gdf = geopandas.GeoDataFrame(
+        df2, geometry=geopandas.points_from_xy(df2['dlug'], df2['szer']), crs="EPSG:4326"
+    )
+    # ax.set_title("Wybrane miasta")
+    gdf.plot(ax=ax, color="red")
+    canvas.draw()
+    ilosc_miast.config(text="Ilość miast: " + str(len(df2)) + " / " + str(len(df)))
+
+def odgadniete_miasta():
+    lista_miast.config(text="Ostatnie odgadnięte miasta: " + ", ".join(df2['miasto'].head(10).unique()))
+
 
 # Przycisk
-tk.Button(window, text="Pokaż na mapie", command=pokaz_miasto).pack(pady=5)
+custom_font = font.Font(family="Helvetica", size=12, weight="bold")
+button_options = {
+    "font": custom_font,
+    "bg": "#4CAF50",
+    "fg": "white",
+    "activebackground": "#45a049",
+    "activeforeground": "white",
+    "bd": 2,
+    "relief": "raised",
+    "padx": 10,
+    "pady": 5
+}
+
+tk.Button(window, text="Powrót do mapy", command=powrot, **button_options).place(x=1200, y=250)
+tk.Button(window, text="Pokaz brakujace", command=pokaz_brakujace, **button_options).place(x=1200, y=200)
 
 # Obsługa klawisza Enter
 window.bind("<Return>", pokaz_miasto_enter)
 width = window.winfo_screenwidth()
 height = window.winfo_screenheight()
 window.geometry("%dx%d" % (width, height))
+tk.Button(window,text="Wyświetlanie po populacji", command=pokaz_populacje, **button_options).place(x=100, y=300)
+tk.Button(window, text="Pokaz odgadniete", command=odgadniete_miasta,**button_options).place(x=1200, y=100)
 # Start GUI
 window.mainloop()
